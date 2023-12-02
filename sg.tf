ed46e66a90cf03d52a9e6736e05609574e5c588f
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2023-12-02 11:12:59 +0300

    add fix sg

--- sg.tf
+++ sg.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "sg_main" {
   description = "14558"
   vpc_id = aws_vpc.vpc_main.id 
   egress {
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [aws_vpc.vpc_main.cidr_block]
     from_port   = 0
     protocol    = "-1"
     to_port     = 0
@@ -150,7 +150,7 @@ resource "aws_security_group" "db_sg" {
     to_port          = 0
     protocol         = "-1"
     #cidr_blocks      = [aws_vpc.vpc_main.cidr_block]
-    cidr_blocks =     ["0.0.0.0/0"] 
+    cidr_blocks =     [aws_vpc.vpc_main.cidr_block]
   }
   tags = {
     name = "${var.prefix}-db-sec-group"
